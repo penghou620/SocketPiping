6265df0d8ec74b8f4988cd50fe9b08905059b997
peng hou <dustsnow@gmail.com>
2013-06-08 04:00:08 -0400

    Doing: combinating server.c and fork.c

--- experimentLab/fork.c
+++ experimentLab/fork.c
@@ -31,46 +31,86 @@ main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("pc = [%d, %d]\n", pc[0], pc[1]);
-    printf("cp = [%d, %d]\n", cp[0], cp[1]);
+    // printf("pc = [%d, %d]\n", pc[0], pc[1]);
+    // printf("cp = [%d, %d]\n", cp[0], cp[1]);
 
     c1pid = fork();
+    printf("PID is %d\n",c1pid);
     if (c1pid == -1) {
         perror("fork1");
         exit(EXIT_FAILURE);
     }
     if (c1pid == 0) {    
-        printf("Child\n");
-        
-        close(cp[READ]);    
-        dup2(cp[WRITE],OUT);
-        close(cp[WRITE]);
-
-        close(pc[WRITE]);
-        dup2(pc[READ],IN);
-        close(pc[READ]);
-
-        // while(read(IN,&buf1,1) > 0)
-        execl("/bin/gzip","gzip",NULL); 
- 
-    } else {            
-        printf("Parent\n");
-
-        close(pc[READ]);
-        write(pc[WRITE],"abcdefghij\n",11);
-        close(pc[WRITE]);
-
-        close(cp[WRITE]);
-
-        dup2(cp[READ],IN);
-        close(cp[READ]);
-
-        execl("/bin/gzip","gzip","-df",NULL); 
+        printf("First Child\n");
+        printf("PID is %d\n",c1pid);
+        // close(cp[READ]);    
+        // dup2(cp[WRITE],OUT);
+        // close(cp[WRITE]);
 
         // close(pc[WRITE]);
+        // dup2(pc[READ],IN);
+        // close(pc[READ]);
+
+        // while(read(IN,&buf1,1) > 0)
+        // execl("/bin/gzip","gzip",NULL); 
+ 
+    } else {            
+        printf("Parent of child 1\n");
+        printf("PID is %d\n",c1pid);
+        // close(pc[READ]);
+        // write(pc[WRITE],"abcdefghij\n",11);
+        // close(pc[WRITE]);
+
+        // close(cp[WRITE]);
+
+        // dup2(cp[READ],IN);
+        // close(cp[READ]);
+
+        // execl("/bin/gzip","gzip","-df",NULL); 
+
+        // // close(pc[WRITE]);
         exit(EXIT_SUCCESS);
 
     }
+    c2pid = fork();
+    if (c2pid == -1) {
+        perror("fork2");
+        exit(EXIT_FAILURE);
+    }
+    if (c2pid == 0) {    
+        printf("Second Child Process\n");
+        
+        // close(cp[READ]);    
+        // dup2(cp[WRITE],OUT);
+        // close(cp[WRITE]);
+
+        // close(pc[WRITE]);
+        // dup2(pc[READ],IN);
+        // close(pc[READ]);
+
+        // // while(read(IN,&buf1,1) > 0)
+        // execl("/bin/gzip","gzip",NULL); 
+        // exit(EXIT_SUCCESS);
+ 
+    } else {            
+        printf("Parent of child 2\n");
+
+        // close(pc[READ]);
+        // write(pc[WRITE],"abcdefghij\n",11);
+        // close(pc[WRITE]);
+
+        // close(cp[WRITE]);
+
+        // dup2(cp[READ],IN);
+        // close(cp[READ]);
+
+        // execl("/bin/gzip","gzip","-df",NULL); 
+
+        // // close(pc[WRITE]);
+        // exit(EXIT_SUCCESS);
+
+    }
+        printf("PID is %d\n",c1pid);
         printf("Finish\n");
     return 0;
 }
