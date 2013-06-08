bda167e9d2943c7c77ae8f9646b6d55d739b3f69
peng hou <dustsnow@gmail.com>
2013-06-07 22:39:41 -0400

    Succ: exec in child process and get stdout of child process in parent process

---
+++ experimentLab/fork.c
@@ -0,0 +1,130 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int
+main(int argc, char *argv[])
+{
+    int pipe1fd[2];
+    int pipe2fd[2];
+    int pc[2];
+    int cp[2];
+    
+    
+    pid_t c1pid;
+    pid_t c2pid;
+    char buf1;
+    char buf2;
+    char ch;
+
+   if (pipe(pc) == -1) {
+        perror("pipe pc");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(cp) == -1) {
+        perror("pipe cp");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(pipe1fd) == -1) {
+        perror("pipe1");
+        exit(EXIT_FAILURE);
+    }
+    if (pipe(pipe2fd) == -1) {
+        perror("pipe2");
+        exit(EXIT_FAILURE);
+    }
+
+    c1pid = fork();
+    // printf("cpid is %d\n", cpid);
+    // printf("cpid is %d\n", cpid);
+    if (c1pid == -1) {
+        perror("fork1");
+        exit(EXIT_FAILURE);
+    }
+    if (c1pid == 0) {    
+        printf("Child\n");
+
+        dup2(cp[1],1);
+        dup2(pc[0],0);
+        close(pc[1]);
+        close(cp[0]);
+        execl ("/bin/ls", "ls", "-1", (char *)0);
+        printf("Writing\n");
+
+
+        // close(1);
+        // dup(cp[1]);
+        // close(0);
+        // dup(pc[0]);
+        // close(pc[1]);
+        // close(cp[0]);
+        // execl("/bin/ls","ls","-l",(char *)0);
+        // perror("No exec");
+        // exit(1);
+
+        // close(pipe1fd[0]);
+        // write(pipe1fd[1],argv[1],strlen(argv[1]));
+        // close(pipe1fd[1]);
+        // printf("Writing\n");
+      
+
+
+
+        // _exit(EXIT_SUCCESS);
+        // while (read(pipefd[0], &buf, 1) > 0)
+        //     write(STDOUT_FILENO, &buf, 1);
+        // write(STDOUT_FILENO, "\n", 1);
+        // close(pipefd[0]);
+        // _exit(EXIT_SUCCESS);
+
+        // printf("    cpid is %d\n", cpid);
+    } else {            /* Parent writes argv[1] to pipe */
+        printf("Parent\n");
+
+        close(cp[1]);
+        while(read(cp[0],&buf1,1)>0)
+        {
+            write(1,&buf1,1);
+        }
+        exit(EXIT_SUCCESS);
+
+
+
+        // printf("Input to child:\n");
+        // while(read(0,&ch,1) >0)
+        // {
+        //     write(pc[1],&ch,1);
+        //     write(1,&ch,1);
+        // }
+        // close(pc[1]);
+        // printf("\nOutput from child:\n");
+        // close(cp[1]);
+        // while(read(cp[0],&ch,1)==1)
+        // {
+        //     write(1,&ch,1);
+        // }
+        // exit(0);
+
+
+        // close(pipe1fd[1]);
+        // while(read(pipe1fd[0],&buf1,1)>0)
+        // {
+        //     printf("\nReading\n");
+        //     write(STDOUT_FILENO,&buf1,1);
+        // }
+        // close(pipe1fd[0]);
+        // exit(EXIT_SUCCESS);
+
+
+
+        // close(pipefd[0]);          /* Close unused read end */
+        // write(pipefd[1], argv[1], strlen(argv[1]));
+        // close(pipefd[1]);          /* Reader will see EOF */
+        // wait(NULL);                /* Wait for child */
+        // exit(EXIT_SUCCESS);
+        // printf("    cpid is %d\n", cpid);
+    }
+        printf("Finish\n");
+    return 0;
+}
--- server.c
+++ server.c
@@ -22,11 +22,86 @@ int main(int argc, char **argv)
 	servaddr.sin_port = htons(5000);
 	bind(listenfd,(struct sockaddr *)&servaddr, sizeof(servaddr));
 	listen(listenfd, 1024);
+
+	//Create pipes; 
+	//xPipefd[0] refers to the read end
+	//xPipefs[1] refers to the write end
+	int inPipefd[2];
+	int outPipefd[2];
+	char inBuffer;
+	char outBuffer;
+
+	if(pipe(inPipefd) == -1){
+		printf("input Pipe creating error");
+		return -1;
+	}
+	if(pipe(outPipefd) == -1){
+		printf("output Pipe creating error");
+		return -1;
+	}
+
+
+	/*
+	 * Duplicate FD to connect two ends of input Pipe
+	 */ 
+	dup2(inPipefd[0],listenfd);//read in data from socket
+	dup2(inPipefd[1],stdout);//pipe out compressed data from xz
+
+	dup2(outPipefd[0],stdin);//read in data from xz (xz give compressed data to stdout, pipe read in from stdin)
+	dup2(outPipefd[1],listenfd);//pipe out to socket
+
 	for(;;){
 		connfd = accept(listenfd,(struct sockaddr *)NULL,NULL);
 		printf("Connected");
 		ticks = time(NULL);
 		write(connfd,ctime(&ticks),strlen(ctime(&ticks)));
+
+		/*
+		 * When data arrives, compress it with xz,
+		 * the output of xz goes to stdout
+		 */
+		 if(connfd == -1)
+		 {
+			printf("acception error");
+		 }
+		 else
+		 {
+		 	/*
+		 	 * Fork children processes
+		 	 */
+		 	//Child Process 1
+			pid_t child1Pid;
+			child1Pid = fork();
+			if(child1Pid == -1)
+			{
+				printf("fork error\n");
+				return -1;
+			}
+			if(child1Pid == 0)
+			{
+
+			}
+			else
+			{
+
+			}
+			
+		 	//Child Process 2
+			pid_t child2Pid;
+			child2Pid = fork();
+			if(child2Pid == -1){
+				printf("fork error\n");
+				return -1;
+			}
+			if(child1Pid == 0)
+			{
+
+			}
+			else
+			{
+				
+			}
+		 }
 		close(connfd);
 	}
 }
