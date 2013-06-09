91e3a57626924ae8e28752c2f7023b54e6b7430e
peng hou <dustsnow@gmail.com>
2013-06-08 20:05:32 -0400

    Done: replace server.c and client.c with eric's files

--- mainwork/src/client.c
+++ mainwork/src/client.c
@@ -1,42 +1,140 @@
-#include "stdio.h"
-#include "strings.h"
-#include "sys/socket.h"
 #include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
 #include <arpa/inet.h>
-#include "sys/types.h"
-#include "stdlib.h"
-#include "unistd.h"
+#include <string.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include "timer.h"
 
+#define SERVER_MSG "[tcp-client] "
+#define DEBUG_PRINT(fmt, ...) printf(SERVER_MSG fmt, ##__VA_ARGS__)
 
-int main(int argc, char **argv)
+void usage() {
+  printf(
+    "usage: tcp-client [OPTIONS] FILE\n"
+    "\n"
+    "OPTIONS:\n"
+    "  -a                                IP address\n"
+    "  -p                                Port number\n"
+    "  -H                                See this message\n"
+    );
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
-	int sockfd;
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		printf("socket error");		
-	struct sockaddr_in servaddr;
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(5000);
-	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-	{
-		printf("inet_pton error\n");
-		return -1;
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char** argv) {
+	/* 1. cmd line args */
+	char *addr = "127.0.0.1";
+	char *port = "4500";
+	char *fname = 0;
+	int opt, option_index;
+	while( (opt = getopt(argc, argv, "a:p:H")) != -1 ) {
+		switch(opt) {
+		case 'a': addr = optarg; break;
+		case 'p': port = optarg; break;
+		case 'H': usage(); exit(0); break;
+		default:  usage(); exit(1); break;
+		}
 	}
-	if(connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
-	{
-		printf("Connect Error\n");
+	while (optind < argc) {
+		if (!fname) {
+		  fname = argv[optind++];
+		} else {
+		  printf("Error: extra arguments on command line.\n\n");
+		  usage();
+		  exit(1);
+		}
 	}
-	int n;
-	const int MAXLINE = 1024;
-	char recvline[MAXLINE];
-	int sum = 0;
-	while((n = read(sockfd,recvline,MAXLINE)) >0)
-	{
-		recvline[n] = 0;
-		if(fputs(recvline,stdout) == EOF)
-			printf("fputs error");
-	}	
-	if(n < 0)
-		printf("read error");
-	return 0;
+	if (!fname) {
+    	printf("Error: Please supply a file!\n\n");
+    	usage();
+    	exit(1);
+  	}
+	DEBUG_PRINT("input address = %s\n", addr);
+	DEBUG_PRINT("input port    = %s\n", port);
+	DEBUG_PRINT("file name 	   = %s\n", fname);
+	DEBUG_PRINT("\n");
+
+	/* 2. data structures */
+	struct addrinfo hints, *server_info;
+	memset(&hints, 0, sizeof(hints));
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	/* 3. info */
+	int rv;
+	if( (rv = getaddrinfo(addr, port, &hints, &server_info)) != 0) {
+		DEBUG_PRINT("Error: getaddrinfo: %s\n", gai_strerror(rv));
+		exit(EXIT_FAILURE);
+	}
+
+	/* 4. server socket */
+	int server_socket;
+	struct addrinfo *p;
+	for(p = server_info; p != NULL; p = p->ai_next) {
+		if( (server_socket = socket(p->ai_family, p->ai_socktype,
+									p->ai_protocol)
+			) == -1 ) {
+			DEBUG_PRINT("Error: Failed to open socket\n");
+			continue;
+		}
+		if(connect(server_socket, p->ai_addr, p->ai_addrlen) == -1) {
+			close(server_socket);
+			DEBUG_PRINT("Error: Failed to connect socket\n");
+			continue;
+		}
+		break;
+	}
+	if(p == NULL) {
+		DEBUG_PRINT("Error: Client failed to connect!\n");
+		exit(EXIT_FAILURE);
+	}
+	char ip[50];
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *) p->ai_addr),
+			  ip, sizeof(ip));
+	DEBUG_PRINT("Connected to %s\n", ip);
+
+	freeaddrinfo(server_info);
+
+	/* 5. file */
+	FILE *client_file = fopen(fname, "r");
+	if(!client_file) {
+		DEBUG_PRINT("Error: Failed to open %s\n", fname);
+		exit(EXIT_FAILURE);
+	}
+
+	/* 6. send */
+	int max_len = 1024;
+	char packet[max_len];
+	int packet_len;
+	char ack[max_len];
+	int ack_len;
+	int bytes_sent;
+	int bytes_total = 0;
+	start_timeofday();
+	start_timer();
+	while( (packet_len = fread(packet, sizeof(char), max_len, client_file)) > 0 ) {
+		bytes_sent = send(server_socket, packet, packet_len, 0);
+		//ack_len = recv(server_socket, ack, max_len, 0);
+		bytes_total += bytes_sent;
+	}
+	DEBUG_PRINT("Transfered %d bytes\n", bytes_total);
+	DEBUG_PRINT("Elapsed time %f\n", end_timer());
+	DEBUG_PRINT("Elapsed time %f\n", end_timeofday());
+
+	/* 7. shutdown */
+	close(server_socket);
 }
--- mainwork/src/server.c
+++ mainwork/src/server.c
@@ -1,119 +1,132 @@
-#include <time.h>
-#include "stdio.h"
-#include "strings.h"
-#include "sys/socket.h"
 #include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
 #include <arpa/inet.h>
-#include "sys/types.h"
-#include "stdlib.h"
-#include "unistd.h"
+#include <string.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdlib.h>
 
-#define STDOUT 1
-#define STDIN 1
-#define READ 0
-#define WRITE 1
+#define SERVER_MSG "[tcp-server] "
+#define DEBUG_PRINT(fmt, ...) printf(SERVER_MSG fmt, ##__VA_ARGS__)
 
-int main(int argc, char **argv)
-{
-	int MAXLINE = 1024;
-	int listenfd, connfd;
-	struct sockaddr_in servaddr;
-	char buff[MAXLINE];
-	time_t ticks;
-	listenfd = socket(AF_INET, SOCK_STREAM,0);
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(5000);
-	bind(listenfd,(struct sockaddr *)&servaddr, sizeof(servaddr));
-	listen(listenfd, 1024);
+void usage() {
+  printf(
+    "usage: tcp-server [OPTIONS] FILE\n"
+    "\n"
+    "OPTIONS:\n"
+    "  -a                                IP address\n"
+    "  -p                                Port number\n"
+    "  -H                                See this message\n"
+    );
+}
 
-	//Create pipes; 
-	//xPipefd[0] refers to the read end
-	//xPipefs[1] refers to the write end
-	int inPipefd[2];
-	int outPipefd[2];
-	char inBuffer;
-	char outBuffer;
-
-	if(pipe(inPipefd) == -1){
-		printf("input Pipe creating error");
-		return -1;
+int main(int argc, char** argv) {
+	/* 1. cmd line args */
+	char *addr = "127.0.0.1";
+	int port = 4500;
+	char *fname = 0;
+	int opt, option_index;
+	while( (opt = getopt(argc, argv, "a:p:H")) != -1 ) {
+		switch(opt) {
+		case 'a': addr = optarg; break;
+		case 'p': port = atoi(optarg); break;
+		case 'H': usage(); exit(0); break;
+		default:  usage(); exit(1); break;
+		}
 	}
-	if(pipe(outPipefd) == -1){
-		printf("output Pipe creating error");
-		return -1;
+	while (optind < argc) {
+		if (!fname) {
+		  fname = argv[optind++];
+		} else {
+		  printf("Error: extra arguments on command line.\n\n");
+		  usage();
+		  exit(1);
+		}
+	}
+	if (!fname) {
+    	printf("Error: Please supply a file!\n\n");
+    	usage();
+    	exit(1);
+  	}
+	DEBUG_PRINT("input address = %s\n", addr);
+	DEBUG_PRINT("input port    = %d\n", port);
+	DEBUG_PRINT("file name 	   = %s\n", fname);
+	DEBUG_PRINT("\n");
+
+	/* 2. data structures */
+	struct sockaddr_in server_sa;
+	memset(&server_sa, 0, sizeof(server_sa));
+
+	server_sa.sin_family = AF_INET;
+	server_sa.sin_port = htons(port);
+	inet_pton(server_sa.sin_family, addr, &server_sa.sin_addr);
+
+	/* 3. TCP */
+	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_socket < 0) {
+		DEBUG_PRINT("Error: Failed to create socket!\n");
+		exit(EXIT_FAILURE);
+	}
+	DEBUG_PRINT("Created socket\n");
+
+	/* 4. bind */
+	if(bind(server_socket, (struct sockaddr *)&server_sa, sizeof(server_sa)) < 0) {
+		DEBUG_PRINT("Error: Failed to bind!\n");
+		exit(EXIT_FAILURE);
+	}
+	DEBUG_PRINT("Bound socket\n");
+
+	/* 5. listen */
+	if(listen(server_socket, 4) < 0) {
+		DEBUG_PRINT("Error: Failed to listen\n");
+		exit(EXIT_FAILURE);
+	}
+	DEBUG_PRINT("Listening for connections\n");
+
+	while(1) {
+		/* 6. accept */
+		struct sockaddr_in client_sa;
+		socklen_t client_len = sizeof(client_sa);
+		int client_socket = accept(server_socket, (struct sockaddr *) &client_sa,
+								   &client_len);
+		if(client_socket < 0) {
+			DEBUG_PRINT("Error: Failed to accept!\n");
+			exit(EXIT_FAILURE);
+		}
+		char client_addr[50];
+		inet_ntop(client_sa.sin_family, &client_sa.sin_addr, client_addr,
+				  sizeof(client_addr));
+		DEBUG_PRINT("Accepted %s\n", client_addr);
+
+		/* 7. file */
+		FILE *server_file = fopen(fname, "w");
+		if(server_file == 0) {
+			DEBUG_PRINT("Error: Failed to open file %s\n", fname);
+			exit(EXIT_FAILURE);
+		}
+
+		/* 8. packet */
+		int max_len = 1024;
+		char packet[max_len];
+		memset(packet, 0, sizeof(char) * max_len);
+
+		/* 9. recv */
+		int packet_len;
+		int bytes_recv = 0;
+		char ack[] = "ACK";
+		while( (packet_len = recv(client_socket, packet, max_len, 0)) > 0 ) {
+			fwrite(packet, sizeof(char), packet_len, server_file);
+			bytes_recv += packet_len;
+			//send(client_socket, ack, 4, 0);
+		}
+		DEBUG_PRINT("Received %d bytes\n", bytes_recv);
+
+		/* 10. shutdown */
+		close(client_socket);
+		fclose(server_file);
 	}
 
-
-	/*
-	 * Duplicate FD to connect two ends of input Pipe
-	 */ 
-	dup2(inPipefd[0],listenfd);//read in data from socket
-	dup2(inPipefd[1],STDOUT);//pipe out compressed data from xz
-
-
-
-
-	/*
-	 * Creating two Child Process
-	 */
-	pid_t sendChildProcess;
-	pid_t receiveChildProcess;
-	sendChildProcess = fork();
-	receiveChildProcess = fork();
-
-	for(;;){
-		connfd = accept(listenfd,(struct sockaddr *)NULL,NULL);
-		printf("Connected");
-		ticks = time(NULL);
-		write(connfd,ctime(&ticks),strlen(ctime(&ticks)));
-
-		/*
-		 * When sending data arrives, compress it, the output of compressor goes to stdout
-		 */
-		 if(connfd == -1)
-		 {
-			printf("acception error");
-		 }
-		 else
-		 {
-		 	/*
-		 	 * Children's processes
-		 	 */
-		 	//sending child process
-			if(sendChildProcess == -1)
-			{
-				printf("fork error\n");
-				return -1;
-			}
-			if(sendChildProcess == 0)
-			{
-				dup2(outPipefd[READ],STDIN);//read in data from xz (xz give compressed data to stdout, pipe read in from stdin)
-				close(outPipefd[READ]);
-				dup2(outPipefd[READ],listenfd);//pipe out to socket	
-				close(outPipefd[WRITE]);
-				execl("/bin/gzip","gzip",NULL);
-			}
-			else
-			{
-				close(outPipefd[READ]);
-			}
-
-		 	//Reveiving Child Process 2
-			if(receiveChildProcess == -1){
-				printf("fork error\n");
-				return -1;
-			}
-			if(receiveChildProcess == 0)
-			{
-
-			}
-			else
-			{
-				
-			}
-		 }
-		close(connfd);
-	}
+	close(server_socket);
 }
